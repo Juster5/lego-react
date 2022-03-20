307e9f09943bc2cae3e661f6e784f59f5d2bd544
zhongchaolin <zhongchaolin@yy.com>
2022-03-20 15:43:58 +0800

    文本框添加弹性居中功能

--- packages/ui/src/ui-component/base/Text/index.tsx
+++ packages/ui/src/ui-component/base/Text/index.tsx
@@ -2,7 +2,8 @@ import React, { memo } from 'react';
 import { ITextConfig } from './schema';
 import logo from '@/assets/text.png';
 const Text = memo((props: ITextConfig & { isTpl: boolean }) => {
-  const { align, text, fontSize, color, lineHeight, isTpl } = props;
+  const { align, text, fontSize, color, lineHeight, isTpl, flexCenter } = props;
+  
   return (
     <>
       {isTpl ? (
@@ -10,7 +11,13 @@ const Text = memo((props: ITextConfig & { isTpl: boolean }) => {
           <img src={logo} alt=""></img>
         </div>
       ) : (
-        <div style={{ color, textAlign: align, fontSize, lineHeight }}>{text}</div>
+        <div style={{ color, textAlign: align, fontSize, lineHeight, height: flexCenter ? '100%': 'auto'}}>
+          {flexCenter ? 
+            <div style={{height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+              {text}
+            </div> 
+            : text}
+        </div>
       )}
     </>
   );
